10b5c9285a6177e04a134379be90dea789e047dc
shocklaing <shockwater@gmail.com>
2019-01-13 14:22:07 +0800

    Basis handle pug template.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  const shock = { name: "Shock", age: 1080, cool: false };
+  // res.send('Hey! It works!');
+  // res.json(shock);
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render('hello', {
+    name: 'Shock',
+    dog: 'up'
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  res.send(req.params.name);
 });
 
 module.exports = router;
