fe383714f4714fbe50f7e54e9e64089ea13e3e56
shocklaing <shockwater@gmail.com>
2019-01-21 21:00:19 +0800

    Creating an editing flow for stores.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -23,3 +23,23 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render("stores", { title: "Stores", stores });
 };
+
+exports.editStore = async (req, res) => {
+  // Find the store givent the id.
+  const store = await Store.findOne({ _id: req.params.id });
+
+  res.render("editStore", { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // find and update the store
+  const store = await Store.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return new store instead of the old one
+    runValidators: true
+  }).exec();
+
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. 
+  <a href="/stores/${store.slug}">View Store</a>`);
+
+  res.redirect(`/stores/${store._id}/edit`);
+};
