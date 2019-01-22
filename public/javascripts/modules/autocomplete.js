9292cf55bb7ff0feb61b36bec1a38ebb843578ee
shocklaing <shockwater@gmail.com>
2019-01-23 01:00:56 +0800

    Geocoding data with google maps.

---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,17 @@
+function autocomplete(input, latInput, lngInput) {
+  if (!input) return;
+  const dropdown = new google.maps.places.Autocomplete(input);
+  dropdown.addListener("place_changed", () => {
+    const place = dropdown.getPlace();
+    console.log(place);
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+
+  // if someome hit enter on the address filed., don't submit the form.
+  input.on("keydown", e => {
+    if (e.keyCode === 13) e.preventDefault();
+  });
+}
+
+export default autocomplete;
