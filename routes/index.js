3ba4ab2bc3acb6eaf2b1c96876248dbdf4980c31
shocklaing <shockwater@gmail.com>
2019-01-26 20:26:40 +0800

    Routing and templating single store.

--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,6 @@ router.post(
   catchErrors(storeController.updateStore)
 );
 router.get("/stores/:id/edit", catchErrors(storeController.editStore));
+router.get("/store/:slug", catchErrors(storeController.getStoreBySlug));
 
 module.exports = router;
