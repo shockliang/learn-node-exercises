96e29fab6400938073ba389f34d51f4f59474691
shocklaing <shockwater@gmail.com>
2019-02-02 18:17:01 +0800

    Virtual fields, login; logout middleware and protecting routes.

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const { catchErrors } = require("../handlers/errorHandlers");
 
 router.get("/", catchErrors(storeController.getStores));
 router.get("/stores", catchErrors(storeController.getStores));
-router.get("/add", storeController.addStore);
+router.get("/add", authController.isLoggedIn, storeController.addStore);
 router.post(
   "/add",
   storeController.upload,
@@ -28,6 +28,7 @@ router.get("/tags", catchErrors(storeController.getStoresByTag));
 router.get("/tags/:tag", catchErrors(storeController.getStoresByTag));
 
 router.get("/login", userController.loginForm);
+router.post("/login", authController.login);
 router.get("/register", userController.registerForm);
 
 router.post(
@@ -36,5 +37,7 @@ router.post(
   userController.register,
   authController.login
 );
+
+router.get("/logout", authController.logout);
 
 module.exports = router;
