2ab5898df6177128ce4f31e5642c8f39c6f14530
shocklaing <shockwater@gmail.com>
2019-02-13 20:06:39 +0800

    Adding a review data model.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require("../controllers/storeController");
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
+const reviewController = require("../controllers/reviewController");
 
 const { catchErrors } = require("../handlers/errorHandlers");
 
@@ -51,6 +52,11 @@ router.post(
 );
 router.get("/map", storeController.mapPage);
 router.get("/hearts", catchErrors(storeController.getHearts));
+router.post(
+  "/reviews/:id",
+  authController.isLoggedIn,
+  catchErrors(reviewController.addReview)
+);
 
 // Api section
 router.get("/api/search", catchErrors(storeController.searchStores));
